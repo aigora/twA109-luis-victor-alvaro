7c5ad45414357da31300c4d7475d30bb18082d2d
victorramosperez <victor.ramos.perez@alumnos.upm.es>
2019-05-03 21:28:01 +0200

    Create Pasando datos a fichero.c

---
+++ Pasando datos a fichero.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void escribir_fichero(int[]);
+int main(void) 
+{
+	int i;
+	int vector[3];
+    //RECIBIR DATOS
+    for (i=0;i<3;i++)
+    {
+    	printf("Inroduzca un numero\n");
+    	scanf("%d",&vector[i]);
+	}
+    printf("Inroduzca 3 numeros\n");
+    escribir_fichero(vector);
+	return 0;
+}
+void escribir_fichero(int vector[])
+{
+	int j;
+	FILE *salida;
+	salida=fopen("Caramelos.txt","wt");
+	if (salida==NULL)
+	    printf("No se han podido guardar los datos\n");
+	else
+	{
+		for(j=0;j<3;j++)
+		{
+			switch (vector[j])
+			{
+				case 1: 
+				    fprintf(salida,"ROJO\n");
+				    break;
+				case 2: 
+				    fprintf(salida,"VERDE\n");
+				    break;
+				case 3: 
+				    fprintf(salida,"AZUL\n");
+				    break;
+			}
+		}
+	}
+}
